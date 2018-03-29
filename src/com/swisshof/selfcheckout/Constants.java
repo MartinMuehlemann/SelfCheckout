c9031d7a215c0a77c635f9b4620b7e0290812890
Martin Muehlemann <martin.muehlemann@csa.ch>
2018-03-29 17:07:42 +0200

    Improved look of InfoPane

--- src/com/swisshof/selfcheckout/Constants.java
+++ src/com/swisshof/selfcheckout/Constants.java
@@ -4,5 +4,8 @@ import java.awt.Color;
 
 public class Constants {
 	public final static Color COLOR_BG = Color.WHITE;
+	public final static Color COLOR_SUCCESS = new Color(0x3bb54a);
+	public final static Color COLOR_FAILURE = new Color(0xea1919);
+	public final static Color COLOR_PROGRESS = Color.BLACK;
 
 }
--- src/com/swisshof/selfcheckout/gui/InfoPane.java
+++ src/com/swisshof/selfcheckout/gui/InfoPane.java
@@ -1,5 +1,8 @@
 package com.swisshof.selfcheckout.gui;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -9,6 +12,8 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import com.swisshof.selfcheckout.Constants;
+import com.swisshof.selfcheckout.IResourceProvider.FontIdentifier;
 import com.swisshof.selfcheckout.IResourceProvider.ImageIdentifier;
 import com.swisshof.selfcheckout.SelfCheckoutContext;
 import com.swisshof.selfcheckout.statemachine.MainStm.Events;
@@ -27,6 +32,8 @@ public class InfoPane extends JPanel {
 	protected JButton button;
 	
 	
+	
+	
 	protected InformationType type = InformationType.INFO_PROGRESS;
 	
 	public InfoPane(SelfCheckoutContext context)
@@ -34,13 +41,30 @@ public class InfoPane extends JPanel {
 		super();
 		this.context = context;
 		
+		Font baseFontRegular = context.getResourceProvider().getFont(FontIdentifier.FrutigerCondensed);	
+		Font baseFontBold = context.getResourceProvider().getFont(FontIdentifier.FrutigerBold);	
+
+		
+		Font fontInfoText = baseFontRegular.deriveFont(Font.PLAIN, 36);		
+		Font fontButton = baseFontBold.deriveFont(Font.PLAIN, 36);
+		
 		GridBagLayout gbl = new GridBagLayout();
 		setLayout(gbl);
 		
+		setBackground(Constants.COLOR_BG);
+		
 		lblIcon = new JLabel();
 		lblInfoText = new JLabel();
-		lblInfoText.setText("infoT text");
+		lblInfoText.setText("info text");
+		lblInfoText.setFont(fontInfoText);
+		
 		button = new JButton("OK");
+		button.setFont(fontButton);
+		button.setPreferredSize(new Dimension(300, 120));
+		button.setBackground(Constants.COLOR_BG);
+		//button.setForeground(Constants.COLOR_SUCCESS);
+
+		
 		button.addActionListener(new ActionListener() {
 			
 			@Override
@@ -59,23 +83,25 @@ public class InfoPane extends JPanel {
 					break;
 				
 				}
-
-				
 			}
 		});
 		
 		GridBagConstraints lc = new GridBagConstraints();
-		lc.fill = GridBagConstraints.BOTH;
 		lc.gridx = 0;
 		lc.gridy = 0;
 
-		
+		lc.anchor = GridBagConstraints.CENTER;
+		lc.weighty = 0.2;
 		add(lblIcon, lc);
 		
 		lc.gridy++;
+		lc.weighty = 0.3;
+		lc.fill = GridBagConstraints.BOTH;
 		add(lblInfoText, lc);
 		
 		lc.gridy++;
+		lc.weighty = 0.3;
+		lc.fill = GridBagConstraints.NONE;
 		add(button, lc);
 		
 	}
@@ -83,34 +109,36 @@ public class InfoPane extends JPanel {
 	public void setInfoText(InformationType type, String infoText) 
 	{
 		this.type = type;
+				
 		switch(type) {
 		case INFO_ERROR:
 			lblIcon.setIcon(context.getResourceProvider().getImage(ImageIdentifier.Failure));
 			button.setText(context.getString("infopane.btn.ok"));
+			lblInfoText.setForeground(Constants.COLOR_FAILURE);
 			break;
 		case INFO_PROGRESS:
 			lblIcon.setIcon(context.getResourceProvider().getImage(ImageIdentifier.Hourglass));
 			button.setText(context.getString("infopane.btn.abort"));
-
+			lblInfoText.setForeground(Constants.COLOR_PROGRESS);
 			break;
 		case INFO_SUCCESS:
 			lblIcon.setIcon(context.getResourceProvider().getImage(ImageIdentifier.Success));
 			button.setText(context.getString("infopane.btn.ok"));
+			lblInfoText.setForeground(Constants.COLOR_SUCCESS);
 			break;
 		default:
 			break;
 		
 		}
 		
-		
-		lblInfoText.setText(infoText);
+		lblInfoText.setText("<html><center>" + infoText + "</center></html>");
 	}
 	
 	public void enableBtnConfirm(boolean enable)
 	{
 		button.setEnabled(enable);
 	}
-	
+
 	private static final long serialVersionUID = 1L;
 
 }
