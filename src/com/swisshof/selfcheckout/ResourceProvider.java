6830d7a2f60a19c7d92c653e938ed2e0e75aba24
Martin Muehlemann <martin.muehlemann@csa.ch>
2019-07-24 13:22:02 +0200

    Software version V2.3

--- src/com/swisshof/selfcheckout/ResourceProvider.java
+++ src/com/swisshof/selfcheckout/ResourceProvider.java
@@ -21,7 +21,7 @@ public class ResourceProvider implements IResourceProvider {
 	
 	private final static String RESOURCE_BASE_PACKAGE = "res/";
 	
-	private final static String FIRMWARE_VERSION = "V2.2";
+	private final static String FIRMWARE_VERSION = "V2.3";
 
 	
 	protected Logger logger = LogManager.getLogger(getClass().getName());
